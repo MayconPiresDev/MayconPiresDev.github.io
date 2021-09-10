dfd0e66f9c5f807dc26fdb3385de22906a97fd8a
Mayco Pires <mayconpires4@gmail.com>
2021-09-10 20:56:59 -0300

    Criação de novos campos public e views

--- Server/server.js
+++ Server/server.js
@@ -1,9 +1,20 @@
 //construção do servidor
 const express = require('express')
+const nunjucks = require('nunjucks')
+
+//configuração da template engine
 const server = express()
 
+server.use(express.static('public'))
+
+nunjucks.configure("views", {
+    express:server
+})
+
+server.set("view engine", "html")
+
 server.get("/", function(req, res) {
-    return res.send("hi!")
+    return res.render("index")
 })
 
 server.listen(5000, function () {
