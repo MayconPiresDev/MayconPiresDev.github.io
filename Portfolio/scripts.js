b62970afd1d3d77be1c220208094fd32ece4261d
Mayco Pires <mayconpires4@gmail.com>
2021-09-09 21:03:15 -0300

    Criação de scripts para melhor funcionalidade

---
+++ Portfolio/scripts.js
@@ -0,0 +1,13 @@
+//manipulação de lementos da pagina
+const modalOverlay = document.querySelector('.modal-overlay');
+const cards = document.querySelectorAll('.card');
+
+for (let card of cards) {
+    card.addEventListener("click", function() {
+        modalOverlay.classList.add('active');
+    })
+}
+
+document.querySelector(".close-modal").addEventListener("click", function() {
+    modalOverlay.classList.remove('active');
+})
